064543fb7a6b9dad419fa9157bc7fea4465a1c57
Francisco Javier <fjsuast@gmail.com>
2019-08-16 22:59:58 -0500

    Se generó un nuevo componente compartido sidebar en la carpeta shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HeaderComponent } from './shared/header/header.component';
     DashboardComponent,
     ProgressComponent,
     Graficas1Component,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
