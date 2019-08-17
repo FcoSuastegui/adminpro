4842c285389dbb2be596b24813bd18c8394bfebe
Francisco Javier <fjsuast@gmail.com>
2019-08-16 22:50:53 -0500

    Se genero un nuevo componente pagina no encontrada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule
